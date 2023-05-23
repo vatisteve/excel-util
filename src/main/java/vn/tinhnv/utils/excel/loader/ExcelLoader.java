4ca7a4748b01a30f6f7984de8af63603da41e053
tinhnv <tinhnv@greenglobal.vn>
2023-05-23 17:45:41 +0700

    init project
    mvn archetype:generate -DgroupId=vn.tinhnv.utils -DartifactId=utils -DarchetypeArtifactId=maven-archetype-quickstart -DarchetypeVersion=1.4 -DinteractiveMode=false
    update schema location
    use java 1.8
    use junit test 5
    init excel loader package, class, test class

---
+++ src/main/java/vn/tinhnv/utils/excel/loader/ExcelLoader.java
@@ -0,0 +1,10 @@
+package vn.tinhnv.utils.excel.loader;
+
+/**
+ * @author Steve
+ * @since May 23, 2023
+ *
+ */
+public interface ExcelLoader {
+
+}
---
+++ src/test/java/vn/tinhnv/utils/AbstractUtilsTest.java
@@ -0,0 +1,5 @@
+package vn.tinhnv.utils;
+
+public abstract class AbstractUtilsTest {
+
+}
