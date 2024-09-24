14880a3cb72b46b5973f032940c59158b681538b
tinhnv <tinhnv@greenglobal.vn>
2024-09-24 20:07:16 +0700

    update excel writer interface javadoc

--- src/main/java/io/github/vatisteve/utils/excel/writer/ExcelWriterConfiguration.java
+++ src/main/java/io/github/vatisteve/utils/excel/writer/ExcelWriterConfiguration.java
@@ -8,7 +8,9 @@ import java.time.ZoneId;
 public interface ExcelWriterConfiguration {
 
     /**
-     * Sheet name
+     * Define the sheet name base on its index
+     * @param index Sheet index
+     * @return Sheet name
      */
     default String sheetName(int index) {
         return String.format("Data %d", index);
@@ -16,6 +18,7 @@ public interface ExcelWriterConfiguration {
 
     /**
      * Default format for local time instance
+     * @return Time string format {@code HH:mm:ss}
      */
     default String timeFormat() {
         return "HH:mm:ss";
@@ -23,6 +26,7 @@ public interface ExcelWriterConfiguration {
 
     /**
      * ZoneId default when convert ZoneDateTime to LocalDateTime
+     * @return the system default zone id
      */
     default ZoneId zoneId() {
         return ZoneId.systemDefault();
@@ -30,6 +34,8 @@ public interface ExcelWriterConfiguration {
 
     /**
      * Default cell style
+     * @param activeWb  Active workbook to create cell style
+     * @return a new instance cell style with no options
      */
     default CellStyle cellStyle(Workbook activeWb) {
         return activeWb.createCellStyle();
@@ -37,6 +43,8 @@ public interface ExcelWriterConfiguration {
 
     /**
      * Excel Header
+     * @param activeWb  Active workbook to create excel header and its style
+     * @return {@link ExcelHeader}
      */
     default ExcelHeader excelHeader(Workbook activeWb) {
         return null;
@@ -44,7 +52,8 @@ public interface ExcelWriterConfiguration {
 
     /**
      * Row height
-     * @see org.apache.poi.ss.usermodel.Row#setHeight(short) 
+     * @see org.apache.poi.ss.usermodel.Row#setHeight(short)
+     * @return {@code -1} by default for row height
      */
     default short rowHeight() {
         return -1;
